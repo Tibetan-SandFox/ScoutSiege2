aa93466e0b81428fe83ccf1c6c2e652736351a4a
Mikawa0131 <mikawayuki0131@gmail.com>
2021-06-02 11:23:02 +0900

    カメラ追従

---
+++ Assets/C#Script/FollowCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        //カメラとプレイヤーの位置を同じにする
+        transform.position = new Vector3(playerPos.x, playerPos.y, -10);
+    }
+}
